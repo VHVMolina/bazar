57a10271a87f28f0568d38fa71c66288e0d08a37
VicTech444 <Victor_verq@outlook.com>
2024-06-10 14:21:15 -0500

    change env name for supabase key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,5 +4,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = 'https://tiqlaateuguyviwaumpm.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
