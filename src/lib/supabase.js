5f53c05997724614deba140a26d4f121934884a3
VicTech444 <Victor_verq@outlook.com>
2024-06-10 14:23:45 -0500

    fix supabase env key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,5 +4,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = 'https://tiqlaateuguyviwaumpm.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
